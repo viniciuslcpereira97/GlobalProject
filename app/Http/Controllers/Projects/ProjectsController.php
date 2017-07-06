0a5a1b4d7d3546205a1aba107afaab1de27ddf06
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-06 14:54:59 -0300

    projects controlllers folder

---
+++ app/Http/Controllers/Projects/ProjectsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DevFinder\Http\Controllers\Projects;
+
+use Illuminate\Http\Request;
+use DevFinder\Http\Controllers\Controller;
+
+class ProjectsController extends Controller
+{
+    //
+}
