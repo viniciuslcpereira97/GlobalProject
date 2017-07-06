d7fd682eaf4f6a4ea21495224e017fc80e509f5f
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-07-06 14:49:31 -0300

    auth controllers

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace DevFinder\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use DevFinder\User;
+use DevFinder\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace DevFinder\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use DevFinder\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
